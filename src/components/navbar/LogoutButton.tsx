a16930d7c0e53f273b4fd251254ae02462eef340
Blake Wood Jr <blakewoodjr84@gmail.com>
2024-08-13 14:44:22 -0700

    Add logout button functionality.

--- src/components/navbar/LogoutButton.tsx
+++ src/components/navbar/LogoutButton.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { logoutServerAction } from "@/lib/server_actions/logout";
 
 export function LogoutButton() {
   return (
     <form>
-      <Button variant="ghost">Logout</Button>
+      <Button variant="ghost" formAction={logoutServerAction}>
+        Logout
+      </Button>
     </form>
   );
 }
