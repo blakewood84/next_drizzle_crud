8c7b16fc7f5a9510f6f098cd06d03abc0f6d32ed
Blake Wood Jr <blakewoodjr84@gmail.com>
2024-08-12 19:13:25 -0700

    Add schema

---
+++ src/db/schema.ts
@@ -0,0 +1,27 @@
+import { integer, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users_table", {
+  id: serial("id").primaryKey(),
+  first_name: text("first_name").notNull(),
+  last_name: text("last_name").notNull(),
+  email: text("email").notNull().unique(),
+});
+
+export const notesTable = pgTable("notes_table", {
+  id: serial("id").primaryKey(),
+  title: text("title"),
+  content: text("content"),
+  userId: integer("user_id")
+    .notNull()
+    .references(() => usersTable.id, { onDelete: "cascade" }),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .$onUpdate(() => new Date()),
+});
+
+export type InsertUser = typeof usersTable.$inferInsert;
+export type SelectUser = typeof usersTable.$inferSelect;
+
+export type InsertNote = typeof notesTable.$inferInsert;
+export type SelectNote = typeof notesTable.$inferSelect;
